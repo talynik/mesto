214d4315de417cd209d34e66cb986c577438848e
talynik <talynik@yandex.by>
2020-10-10 11:55:26 +0300

    project 4 rev 2

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const popup = document.querySelector(".popup");
 const buttonOpenPopup = document.querySelector(".profile__edit-button");
 const buttonAdd = document.querySelector(".profile__add-button");
 const buttonClose = document.querySelector(".popup__close");
-const buttonSubmit = document.querySelector(".popup__submit-button");
+const popapForm = document.querySelector(".popup__form");
 
 const popupToggle = () => {
   popup.classList.toggle("popup_opened");
@@ -11,13 +11,18 @@ const popupToggle = () => {
 buttonOpenPopup.addEventListener("click", popupToggle);
 buttonClose.addEventListener("click", popupToggle);
 
-const popupTextContent = () => {
-  let profileName = document.querySelector(".profile__name");
-  let profileDescription = document.querySelector(".profile__description");
-  let inputs = document.querySelectorAll('input');
-  profileName.textContent = inputs[0].value;
-  profileDescription.textContent = inputs[1].value;
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
+let inputsName = document.querySelector('#name');
+let inputDiscription = document.querySelector('#description');
+inputsName.setAttribute('value', profileName.textContent);
+inputDiscription.setAttribute('value', profileDescription.textContent);
+
+const popupTextContent = (event) => {
+  event.preventDefault();
+  profileName.textContent = inputsName.value;
+  profileDescription.textContent = inputDiscription.value;
+  popupToggle();
 }
 
-buttonSubmit.addEventListener("click", popupTextContent);
-buttonSubmit.addEventListener("click", popupToggle);
+popapForm.addEventListener("submit", popupTextContent);
