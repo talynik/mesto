896a066ea6816b3f907bb30a8a8bfdfdff20ce22
Анатолий <69402978+talynik@users.noreply.github.com>
2021-02-20 08:43:42 +0300

    project 9 deleteTrash

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,41 +12,41 @@ export default class Card {
   _getTemplate() {
     const elementMesto = document.querySelector(this._templateCard).cloneNode(true).content;
     elementMesto.querySelector(".element__number-fanat").textContent = this._likes;
-    if (this._owner != this._idUser) {
-      elementMesto.querySelector(".element__button-delete").remove();
-    }
+
     return elementMesto;
+  }
+
+  _deleteTrash() {
+    if (this._owner != this._idUser) {
+      this._card.querySelector(".element__button-delete").remove();
+    }
   }
 
   generateCard() {
     this._card = this._getTemplate();
-    this._likeEventListener();
-    /* this._deleteEventListener(); */
-    this._generatePopapImage();
-
+    
     this._picture = this._card.querySelector('.element__image');
     this._picture.src = this._link;
     this._picture.alt = this._name;
     this._card.querySelector('.element__name').textContent = this._name;
+
+    this._setEventListener();
+    this._deleteTrash();
   
     return this._card;
   }
 
-  _likeEventListener() {
+  _setEventListener() {
     const like = this._card.querySelector('.element__like');
-    like.addEventListener('click', function () {
+    like.addEventListener('click', () => {
       like.classList.toggle('element__like_active');
     });
-  }
 
-  _deleteEventListener() {
     const deleteButton = this._card.querySelector('.element__button-delete');
-    deleteButton.addEventListener('click', function () {
+    deleteButton.addEventListener('click', () => {
       deleteButton.closest('.element').remove();
     });
-  }
 
-  _generatePopapImage() {
     this._card.querySelector(".element__image").addEventListener("click", () => {
       this._handleCardClick(this._name, this._link);
     });
